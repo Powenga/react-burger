e8f5ae2ac6e3c9473d1b0c783c34150b0dbd1cee
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-10-12 09:36:06 +0300

    fix: replace BurgerIngredients with function

--- src/components/BurgerIngredients/BurgerIngredients.js
+++ src/components/BurgerIngredients/BurgerIngredients.js
@@ -1,41 +1,36 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './BurgerIngredients.module.css';
 import IngredientsSelector from '../IngredientsSelector/IngredientsSelector';
 import IngredientsContainer from '../IngredientsContainer/IngredientsContainer';
 import { ingredientPropTypes } from '../../utils/prop-types';
 
-export default class BurgerIngredients extends React.Component {
-  render() {
-    return (
-      <section className={`${styles.ingredients}`}>
-        <h2 className="text text_type_main-large mt-10 mb-5">
-          Соберите бургер
-        </h2>
-        <IngredientsSelector />
-        <div className={styles.container}>
-          <IngredientsContainer
-            title="Булки"
-            ingredients={this.props.ingredients.filter(
-              (ingredient) => ingredient.type === 'bun'
-            )}
-          />
-          <IngredientsContainer
-            title="Соусы"
-            ingredients={this.props.ingredients.filter(
-              (ingredient) => ingredient.type === 'sauce'
-            )}
-          />
-          <IngredientsContainer
-            title="Начинки"
-            ingredients={this.props.ingredients.filter(
-              (ingredient) => ingredient.type === 'main'
-            )}
-          />
-        </div>
-      </section>
-    );
-  }
+export default function BurgerIngredients({ ingredients, ...props }) {
+  return (
+    <section className={`${styles.ingredients}`}>
+      <h2 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h2>
+      <IngredientsSelector />
+      <div className={styles.container}>
+        <IngredientsContainer
+          title="Булки"
+          ingredients={ingredients.filter(
+            (ingredient) => ingredient.type === 'bun'
+          )}
+        />
+        <IngredientsContainer
+          title="Соусы"
+          ingredients={ingredients.filter(
+            (ingredient) => ingredient.type === 'sauce'
+          )}
+        />
+        <IngredientsContainer
+          title="Начинки"
+          ingredients={ingredients.filter(
+            (ingredient) => ingredient.type === 'main'
+          )}
+        />
+      </div>
+    </section>
+  );
 }
 
 BurgerIngredients.propTypes = {
