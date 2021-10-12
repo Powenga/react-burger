b20c936fab7dafe2df072b6eb42b76a12786ef12
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-10-12 09:30:48 +0300

    fix: remove li wrap from ingredients container

--- src/components/IngredientCard/IngredientCard.js
+++ src/components/IngredientCard/IngredientCard.js
@@ -9,14 +9,14 @@ import styles from './IngredientCard.module.css'
 export default class IngredientCard extends React.Component {
   render() {
     return (
-      <div className={styles.card}>
+      <li className={styles.card}>
         <img src={this.props.image} alt={this.props.name} className="ml-4 mr-4 mb-1"/>
         <Counter count={1} size="default" />
         <p className="text text_type_digits-default mb-1" style={{textAlign: 'center'}}>
           <span style={{verticalAlign: 'top'}}>{this.props.price}</span> <CurrencyIcon type="primary" />
         </p>
         <p className="text text_type_main-default" style={{textAlign: 'center'}}>{this.props.name}</p>
-      </div>
+      </li>
     );
   }
 }
--- src/components/IngredientsContainer/IngredientsContainer.js
+++ src/components/IngredientsContainer/IngredientsContainer.js
@@ -9,9 +9,7 @@ export default class IngredientsContainer extends React.Component {
         <h3 className="text text_type_main-medium mb-6">{this.props.title}</h3>
         <ul className={styles.list}>
           {this.props.ingredients.map((elem) => (
-            <li key={elem._id}>
-              <IngredientCard {...elem} />
-            </li>
+            <IngredientCard key={elem._id} {...elem} />
           ))}
         </ul>
       </>
