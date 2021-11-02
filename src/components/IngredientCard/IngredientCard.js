b86ea1daa175ab8e7d14d5d742ff6277ea91320f
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-11-02 13:55:27 +0300

    feat: add qty func

--- src/components/IngredientCard/IngredientCard.js
+++ src/components/IngredientCard/IngredientCard.js
@@ -3,22 +3,32 @@ import {
   CurrencyIcon,
 } from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 import { useDrag } from 'react-dnd';
+import { useSelector } from 'react-redux';
 import { ingredientPropTypes } from '../../utils/prop-types';
 import styles from './IngredientCard.module.css';
 
 export default function IngredientCard({ ingredient, onIngredientClick }) {
-  const { price, name, image, qty } = ingredient;
+  const { price, name, image, _id } = ingredient;
+  const [qty, setQty] = useState(0);
+  const constructorIngredients = useSelector((store) => [
+    store.burgerConstructor.bun,
+    ...store.burgerConstructor.toppings,
+  ]);
+
+  useEffect(() => {
+    setQty(constructorIngredients.filter((elem) => elem._id === _id).length);
+  }, [_id, constructorIngredients]);
 
   function handleClick() {
     onIngredientClick(ingredient);
   }
 
-
   const [, dragRef] = useDrag({
     type: 'ingredient',
-    item: ingredient
-  })
+    item: ingredient,
+  });
 
   return (
     <li ref={dragRef} onClick={handleClick} className={styles.card}>
