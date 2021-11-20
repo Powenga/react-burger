bf02bed697deae0f6bc8f3e31447d6a076192c71
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-11-20 23:00:19 +0300

    feat: add preloader to ingredient page

--- src/pages/ingredient.js
+++ src/pages/ingredient.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import IngredientDetails from '../components/IngredientDetails/IngredientDetails';
-
+import Preloader from '../components/Preloader/Preloader';
 import styles from '../components/App/App.module.css';
 
 export default function Ingredient() {
+  const { ingredientsRequest } = useSelector(
+    (store) => store.ingredients
+  );
+
+  if (ingredientsRequest) {
+    return <Preloader />;
+  }
 
   return (
     <main className={styles.main}>
