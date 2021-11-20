41c511b27d87b0b8209fe7e2f7a972345aabc2fb
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-11-20 22:54:52 +0300

    feat: add preloader to profile-page

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
+import Preloader from '../../components/Preloader/Preloader';
 
 export function ProtectedRoute({ children, ...rest }) {
   const { isLoggedIn, isUserLoaded } = useSelector((store) => store.user);
 
   if (!isUserLoaded) {
-    return null;
+    return <Preloader />;
   }
 
   return (
