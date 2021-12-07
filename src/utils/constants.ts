88569170734deefcf1437faecaee237692edbc50
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-12-07 13:43:27 +0300

    feat: add types to constants & remove data.js

---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+export const BASE_URL: string = 'https://norma.nomoreparties.space/api';
+export const MODAL_ROOT_SELECTOR: string = '#modal';
+export const ESC_KEY: string = 'Escape';
+
+export const ingredientTypes: string[] = ['bun', 'main', 'sauce'];
+
+export const ACCESS_COOKIE_EXPIRES: number = 20 * 60;
+export const REFRESH_COOKIE_EXPIRES: number = 7 * 24 * 60 * 60;
